190f3511562d45dacffcee01cd9be1de1d4060e2
FreddyPinto <freddypinto@outlook.com>
2023-11-21 22:30:39 -0500

    Add Header and Favorites routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "../pages/home/home";
+import Favorites from "../pages/favorites/favorites";
+import Header from "../components/header/header";
+import { RoutesEnum } from "./routes.enum";
 
 const Routes = () => {
     return (
         <BrowserRouter>
+            <Header />
             <Switch>
-                <Route exact path="/" component={Home} />
+                <Route exact path={RoutesEnum.Home} component={Home} />
+                <Route exact path={RoutesEnum.Favorites} component={Favorites} />
             </Switch>
         </BrowserRouter>
     );
 };
-
 export default Routes;
